257de90ac21a5176f0e7ede5a5bd4baeb4d75306
Reswn <renisuwandi1011@gmail.com>
2025-01-11 10:22:27 +0700

    Membuat Direktori UAS PBW Tech-Blog

---
+++ PBW-IF-VA/UAS_PBW-full-stack-blog/backend/controllers/webhook.controller.js
@@ -0,0 +1,50 @@
+import User from "../models/user.model.js";
+import Post from "../models/user.model.js";
+import { Webhook } from "svix";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const clerkWebhook = async (req, res) => {
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error("CLERK_WEBHOOK_SECRET is not defined");
+  }
+  const payload = req.body;
+  const headers = req.headers;
+
+  const wh = new Webhook(WEBHOOK_SECRET);
+  let evt;
+  try {
+    evt = wh.verify(payload, headers);
+  } catch (err) {
+    res.status(400).json({
+      message: "Invalid signature",
+    });
+  }
+  //   console.log(evt.data);
+  if (evt.type === "user.created") {
+    const newUser = new User({
+      clerkUserId: evt.data.id,
+      username: evt.data.username || evt.data.email_addresses[0].email_address,
+      email: evt.data.email_addresses[0].email_address,
+      img: evt.data.profile_img_url,
+    });
+
+    await newUser.save();
+  }
+
+  if (evt.type === "user.deleted") {
+    const deletedUser = await User.findOneAndDelete({
+      clerkUserId: evt.data.id,
+    });
+
+    await Post.deleteMany({ user: deletedUser._id });
+    await Comment.deleteMany({ user: deletedUser._id });
+  }
+
+  return res.status(200).json({
+    message: "Webhook received",
+  });
+};
